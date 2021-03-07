5c707f0f5d3e96045bdc53925938311b9f235e45
Rakibul615 <rakibul01072019@gmail.com>
2021-03-08 01:09:50 +0600

    cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 const Product = (props) => {
     console.log(props);
@@ -9,12 +11,13 @@ const Product = (props) => {
             </div>
             <div>
                <h4 className="product-name">{props.product.name}</h4>
-               <br/>
-               <p><small>by:{props.product.seller}</small></p>
-               <br/>
+               
+               <p><small>by: {props.product.seller}</small></p>
+               
                <p>price: $ {props.product.price}</p>
-               <br/>
-               <p><small>only {props.product.stock} left in stock - order soon</small></p>
+               
+               <p><small> only {props.product.stock} left in stock - order soon</small></p>
+               <button className="main-button" onClick={()=>props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
 
             </div>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,19 +6,26 @@ import './Shop.css';
 const Shop = () => {
     const first10=fakeData.slice(0,10);
    const [products,setProducts]=useState(first10);
+   const [cart,setCart]= useState([]);
+   const handleAddProduct=(product)=>{
+       console.log("add product",product);
+       const newCart=[...cart,product];
+       setCart(newCart);
+   }
 
     return (
         <div className="shop-container">
             <div className="product-container">
             <ul>
           {
-                products.map(pd => <Product product={pd}></Product>)
+                products.map(pd => <Product product={pd} handleAddProduct={handleAddProduct}></Product>)
           }
           </ul>
 
             </div>
             <div className="cart-container">
                 <h1>this is cart</h1>
+                <h4>order summary: {cart.length}</h4>
             </div>
          
         </div>
