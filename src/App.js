eb5c1b2aec13970984eba44204b36200d34f97af
Rakibul615 <rakibul01072019@gmail.com>
2021-03-13 01:02:31 +0600

    route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {  BrowserRouter as Router,  Switch,  Route,  Link,  useRouteMatch,  useParams} from "react-router-dom";
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+     <Router>
+       <Switch>
+         <Route path="/shop">
+           <Shop></Shop>  
+         </Route>
+         <Route path="/review"> 
+         <Review></Review>
+          </Route>
+         <Route path="/manage">
+            <Inventory></Inventory> 
+            </Route>
+            <Route  exact path="/">
+           <Shop></Shop>  
+         </Route>
+         <Route path="*">
+            <NotFound></NotFound>
+            </Route>
+       </Switch>
+     </Router>
     </div>
   );
 }
